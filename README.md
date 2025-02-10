6ced118e72c3cccc071eae59e14947cd535e6735
Jairam Chitra <jairam.chitra@inventiz.com>
2025-02-10 10:34:08 +0530

    added gitigonore file

---
+++ README.md
@@ -0,0 +1,5 @@
+greet(name:string)
+it returns as Hello ${name}, Welcome Sample greetings Package!
+
+sum(numbersarray:number[])
+it returns as TOTAL = ${value} otherwise Entered a invalid Input
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+export function greet(name) {
+  return `Hello  ${name}, Welcome Sample greetings Package!`;
+}
 
+export function sum(numbersarray) {
+  if (!Array.isArray(numbersarray)) {
+    return "Entered a invalid Input";
+  } else {
+    const value = numbersarray.reduce((acc, curr) => {
+      acc += curr;
+      return acc;
+    }, 0);
+    return `TOTAL = ${value}`;
+  }
+}
